b2c6354360340fd91046e85ad3fad1019b163cc8
1Yevhen1 <genyafedoriv@gmail.com>
2025-06-24 07:38:05 +0300

    goit-js-hw-1

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,10 @@
+
+'use strict';
+
 function getElementWidth(content, padding, border) {
-const totalWidth = content + (2 * padding) + (2 * border);
+const totalWidth = Number.parseFloat(content) + (2 * Number.parseFloat(padding)) + (2 * Number.parseFloat(border));
 return totalWidth;
 }
-
-const content = parseFloat(prompt('Enter the content width: (for example 2px)'));
-const padding = parseFloat(prompt('Enter the padding width: (for example 2px)'));
-const border = parseFloat(prompt('Enter the border width: (for example 2px)'));
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
